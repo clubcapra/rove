8114af9eb832f00c695fb8bab99aa3db71947cc7
SimonR99 <simonroy99@hotmail.ca>
2023-12-06 18:44:23 -0500

    add simulation files

---
+++ src/rove_description/launch/sim.launch.py
@@ -0,0 +1,50 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+import xacro
+
+def generate_launch_description():
+    world_file_name = 'worlds/base_world.world'
+    urdf_file_name = 'urdf/rove.urdf.xacro'
+    world = os.path.join(get_package_share_directory('rove_description'),world_file_name)
+    urdf = os.path.join(get_package_share_directory('rove_description'),urdf_file_name)
+
+    
+    doc = xacro.process_file(urdf)
+    robot_desc = doc.toxml()
+
+    # Gazebo Sim
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    
+    return LaunchDescription([
+        IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': '-r  ' + world + ' --headless-rendering -v4'}.items(),
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc , 'use_sim_time': True}],
+            arguments=[urdf]),
+
+        Node(package='ros_gz_sim', executable='create', arguments=[
+            '-name', 'rove',
+            '-topic', 'robot_description',
+            '-z', '0.1',],
+            output='screen', ),
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=['/model/rove/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+                    '/model/rove/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry'],
+            output='screen'
+        )
+        
+    ])
