19fe92e833717e14a63fc53a34f0707dd376b7e5
patates-cipsi418 <denis.turk2001@outlook.com>
2023-12-09 17:39:23 -0500

    Adding the controller mapping

--- src/rove_bringup/src/rove_controller.cpp
+++ src/rove_bringup/src/rove_controller.cpp
@@ -3,13 +3,39 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 
-enum {
-  JOY_BTN_A = 0,
-  JOY_BTN_B = 1,
-  JOY_BTN_X = 2,
-  JOY_BTN_Y = 3,
-  JOY_BTN_LB = 4,
-  JOY_BTN_RB = 5,
+enum JoyBtn{
+  A = 0,
+  B = 1,
+  X = 2,
+  Y = 3,
+  VIEW = 4,
+  XBOX = 5,
+  MENU = 6,
+  LS = 7,
+  RS = 8,
+  LB = 9,
+  RB = 10,
+  D_UP = 11,
+  D_DOWN = 12,
+  D_LEFT = 13,
+  D_RIGHT = 14,
+//   // Paddles
+//   P_ = 15,
+//   P_ = 16,
+//   P_ = 17,
+//   P_ = 18,
+//   P_ = 19,
+//   P_ = 20,
+//   P_ = 21,
+};
+
+enum JoyStick{
+    LS_X = 0,
+    LS_Y = 1,
+    RS_X = 2,
+    RS_Y = 3,
+    LT = 4,
+    RT = 5,
 };
 
 class RoveController : public rclcpp::Node {
@@ -26,7 +52,9 @@ private:
     // Handle joystick input here
     // For example, print the values of the axes
     for (size_t i = 0; i < joy_msg->axes.size(); ++i) {
-      RCLCPP_INFO(get_logger(), "Axis %zu: %f", i, joy_msg->axes[i]);
+        if(i == LS_X){
+            RCLCPP_INFO(get_logger(), "Axis %zu: %f", i, joy_msg->axes[i]);
+        }
     }
   }
 
