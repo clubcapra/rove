6497220f2cb7719bcee3d71de2ebeaef3f6edb31
SimonR99 <simonroy99@hotmail.ca>
2023-12-11 18:47:55 +0000

    swap config for amazon and default

--- src/rove_description/launch/sim.launch.py
+++ src/rove_description/launch/sim.launch.py
@@ -75,7 +75,7 @@ def generate_launch_description():
         executable='parameter_bridge',
         parameters=[{
             'config_file': os.path.join(pkg_rove_description, 'config',
-                                        'amazon_bridge.yaml'),
+                                        'default_bridge.yaml'),
             'qos_overrides./tf_static.publisher.durability': 'transient_local',
             "use_sim_time": True,
         }],
