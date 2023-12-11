9a5027653d8eea136786f4980c23d517f644c7b0
SimonR99 <simonroy99@hotmail.ca>
2023-12-11 17:30:14 +0000

    fix import and weird space

--- src/rove_description/launch/sim.launch.py
+++ src/rove_description/launch/sim.launch.py
@@ -1,13 +1,14 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
 import os
-import xacro
-from launch.actions import SetEnvironmentVariable
-from launch.actions import TimerAction
-from launch.substitutions import LaunchConfiguration, Command
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 
@@ -28,10 +29,10 @@ def generate_launch_description():
     world = os.path.join(pkg_rove_description, world_file_name)
 
     # robot file
-    rove_urdf_path  =  os.path.join(pkg_rove_description, 'urdf', 'rove.urdf.xacro')
-    robot_desc = ParameterValue(Command(['xacro ', rove_urdf_path]), value_type=str)
-
-
+    rove_urdf_path = os.path.join(pkg_rove_description, 'urdf',
+                                  'rove.urdf.xacro')
+    robot_desc = ParameterValue(Command(['xacro ', rove_urdf_path]),
+                                value_type=str)
     # Setup to launch the simulator and Gazebo world
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -45,14 +46,16 @@ def generate_launch_description():
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'use_sim_time': True, 'robot_description': robot_desc}],
+            parameters=[{'use_sim_time': True,
+                         'robot_description': robot_desc}],
             arguments=[])
-    
+
     # Visualize in RViz
     rviz = Node(
        package='rviz2',
        executable='rviz2',
-       arguments=['-d', os.path.join(pkg_rove_description, 'config', 'basic.rviz')],
+       arguments=['-d', os.path.join(pkg_rove_description, 'config',
+                                     'basic.rviz')],
     )
 
     # Bridge between ROS and Gazebo
@@ -60,12 +63,14 @@ def generate_launch_description():
             package='ros_gz_bridge',
             executable='parameter_bridge',
             parameters=[{
-                'config_file': os.path.join(pkg_rove_description, 'config', 'robot_bridge.yaml'),
-                'qos_overrides./tf_static.publisher.durability': 'transient_local',
+                'config_file': os.path.join(pkg_rove_description, 'config',
+                                            'robot_bridge.yaml'),
+                'qos_overrides./tf_static.publisher.durability':
+                'transient_local',
             }],
             output='screen'
         )
-    
+
     # Spawn robot in Gazebo
     spawn_rove = Node(
         package='ros_gz_sim',
@@ -75,12 +80,13 @@ def generate_launch_description():
                    '-x', '0.0',
                    '-y', '0.0',
                    '-z', '1.0',
-                ],
+                   ],
         output='screen',
     )
-    
+
     return LaunchDescription([
-        SetEnvironmentVariable('IGN_GAZEBO_RESOURCE_PATH', ign_gazebo_resource_path),
+        SetEnvironmentVariable('IGN_GAZEBO_RESOURCE_PATH',
+                               ign_gazebo_resource_path),
         gz_sim,
         bridge,
         robot_state_publisher,
