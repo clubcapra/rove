0cbc0ba4e6268f2b27a2c9f2a247c04ff8ceff4d
SimonR99 <simonroy99@hotmail.ca>
2023-12-11 03:29:45 +0000

    add basic slam

--- src/rove_slam/launch/slam.launch.py
+++ src/rove_slam/launch/slam.launch.py
@@ -3,7 +3,6 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
-
 def generate_launch_description():
 
     pkg_rove_slam = get_package_share_directory('rove_slam')
