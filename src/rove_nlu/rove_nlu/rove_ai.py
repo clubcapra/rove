2a505bf6569b6e4c900d0d7b36fd49ee7fb42f5e
SimonR99 <simonroy99@hotmail.ca>
2024-01-07 23:17:02 +0000

    test using phi-2

---
+++ src/rove_nlu/rove_nlu/rove_ai.py
@@ -0,0 +1,17 @@
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer
+)
+import torch
+
+
+class RoveAI():
+    def __init__(self):
+        self.base_model = "microsoft/phi-2"
+        self.model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype=torch.float32, device_map="cpu", trust_remote_code=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.base_model)
+    
+    def predict(self, message, history):
+        inputs = self.tokenizer.encode(message, return_tensors="pt")
+        outputs = self.model.generate(inputs, max_length=1000, do_sample=True, top_k=50)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
