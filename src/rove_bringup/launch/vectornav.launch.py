df11047dae4991e6266382f5d169b62f5acd7880
patates-cipsi418 <denis.turk2001@outlook.com>
2024-04-14 23:14:25 -0400

    Add launch files to rove bringup

---
+++ src/rove_bringup/launch/vectornav.launch.py
@@ -0,0 +1,25 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    package_name = 'rove_bringup'
+    dir = get_package_share_directory(package_name)
+    # Get params files
+    vn_param_file = os.path.join(dir, 'config', 'vn_300.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='vectornav', 
+            executable='vectornav',
+            output='screen',
+            parameters=[vn_param_file],
+        ),
+        Node(
+            package='vectornav', 
+            executable='vn_sensor_msgs',
+            output='screen',
+            parameters=[vn_param_file],
+        ),
+    ])
