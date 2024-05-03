cba75ce6bd333574cb45f5420c4697baea5b8683
Name <nathan.gt.protic@gmail.com>
2024-05-03 16:50:21 -0400

    overriding zed_ros launch parameters

--- src/rove_zed/launch/zed_body_trck.launch.py
+++ src/rove_zed/launch/zed_body_trck.launch.py
@@ -8,17 +8,21 @@ import os
 
 def generate_launch_description():
 
+    camera_model = 'zed2i'
+
     pkg_zed_wrapper = get_package_share_directory('zed_wrapper')
     launch_file_path = os.path.join(pkg_zed_wrapper, 'launch', 'zed_camera.launch.py')
 
     # Parse yml config file to iterable array
     pkg_rove_zed = get_package_share_directory('rove_zed')
-    config_file_path = os.path.join(pkg_rove_zed, 'config', 'zed_body_trck.yaml')
-    args = yaml.safe_load(Path(config_file_path).read_text()).items()
+    config_override_path = os.path.join(pkg_rove_zed, 'config', 'zed_body_trck.yaml')
 
     return LaunchDescription([
         IncludeLaunchDescription(
             launch_description_source=launch_file_path,
-            launch_arguments=args
+            launch_arguments={ # `ros2 launch rove_zed zed_body_trck.launch.py --show-arguments`
+                'camera_model': camera_model,
+                'ros_params_override_path': config_override_path,
+            }.items()
         )
     ])
