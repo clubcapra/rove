dead2a6cff0e94cb135b7ae8baab305b3d6960d5
nathan-gt <nathan.gt.protic@gmail.com>
2024-06-26 15:20:01 +0200

    constants

--- src/rove_navigation/rove_navigation/behavior/mule_constants.py
+++ src/rove_navigation/rove_navigation/behavior/mule_constants.py
@@ -5,6 +5,8 @@ REWIND = 'REWIND' # Initiates the rewind phase
 PROCESSED_REWIND = 'PROCESSED_REWIND' # Continues the rewind phase until another TPOSE is detected
 PAUSE = 'PAUSE' # Freezes the robot in place (TODO: nice to have, make it still oriente itself towards the target, but not move)
 END = 'END'
+REACH_A = 'REACH_A'
+REACH_B = 'REACH_B'
 
 # poses
 FIGURE_IDLE = "IDLE" # Continue state
