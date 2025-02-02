8e7ee30ece508a2fac85c1c309dbc0dcafb55328
Janice <lockser498jubia@gmail.com>
2025-02-01 20:21:39 -0500

    Add launch folder and file

---
+++ src/rove_radiation/launch/radiation.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    pkg_rove_radiation = get_package_share_directory('rove_radiation')
+
+    return LaunchDescription([
+        Node(
+            package='rove_radiation',
+            executable='radiation_publisher',
+            name='radiation_publisher',
+            output='screen'
+        )
+    ])
+
