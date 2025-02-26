1320929e3fecf6c2a608db29e01083743d246406
Janice <lockser498jubia@gmail.com>
2025-02-25 23:19:33 -0500

    Add basic(with color0 radiation map

--- src/rove_radiation/rove_radiation/radiation_position_tracker.py
+++ src/rove_radiation/rove_radiation/radiation_position_tracker.py
@@ -1,10 +1,18 @@
+import math
+import cv2
+import numpy as np
 import rclpy
 import time
 from rclpy.node import Node
 from std_msgs.msg import Float32
-from geometry_msgs.msg import PoseWithCovarianceStamped, Point
+from geometry_msgs.msg import PoseWithCovarianceStamped, Point, PointStamped
 from nav_msgs.msg import Odometry
 from nav_msgs.msg import OccupancyGrid
+from visualization_msgs.msg import Marker, MarkerArray
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+
 
 
 class RadiationPositionTracker(Node):
@@ -13,51 +21,81 @@ class RadiationPositionTracker(Node):
         
         self.radiation_subscription = self.create_subscription(Float32, '/dose_rate', self.radiation_callback, 10)
         self.odom_subscription = self.create_subscription(Odometry, '/odometry/local', self.localization_pose_callback, 10)
-        self.map_subscription  = self.create_subscription(OccupancyGrid, '/map', self.map_callback, 10)
+        #self.map_subscription  = self.create_subscription(OccupancyGrid, '/map', self.map_callback, 10)
 
-        self.radiation_position_publisher = self.create_publisher(Float32, '/dose_rate', 10)
+        """ self.radiation_position_publisher = self.create_publisher(Float32, '/dose_rate', 10)
         self.radiation_map_publisher = self.create_publisher(OccupancyGrid, '/radiation_map', 10)
+        self.markers_publisher = self.create_publisher(MarkerArray, '/radiation_markers', 10) """
+
+        self.heatmap_publisher = self.create_publisher(Image, "/radiation_map_image", 10)
+
+        self.map_size = 10  #taille de la carte locale (10x10 cellules) (carte locale = champ de vision de la carte de radioactivité du robot...)
+        self.resolution = 0.1  # 10 cm par cellule
+        self.radiation_map_local = np.zeros((self.map_size, self.map_size), dtype=np.float32)
 
         self.current_position = Point(x=0.0, y=0.0, z=0.0)
         self.current_radiation = None
-        self.map = None
 
+        self.bridge = CvBridge()
         self.create_timer(0.5, self.update_publish_map)
-        
+
+        #self.markers_array = MarkerArray()
+               
     def radiation_callback(self, msg):
         self.current_radiation = msg.data
     
     def localization_pose_callback(self, msg):
-        # Update robot's position
         self.current_position.x = msg.pose.pose.position.x
         self.current_position.y = msg.pose.pose.position.y
         self.current_position.z = msg.pose.pose.position.z
-        
-    def map_callback(self, msg):
-        if self.map is None: 
-            self.map = OccupancyGrid()
-            self.map.header = msg.header
-            self.map.info = msg.info
-            self.map.data = [-1] * (msg.info.width * msg.info.height)
-            self.map.data = list(msg.data)
-
-        self.update_publish_map()
 
     def update_publish_map(self):
-        if self.map is not None and self.current_radiation is not None :
-            map_origin = self.map.info.origin.position
-            map_resolution = self.map.info.resolution
-            map_width = self.map.info.width
+        if self.current_radiation is None:
+            return
+        
+        #met a jour la carte locale avec la radiation
+        self.update_local_radiation_map()
 
-            grid_x = int((self.current_position.x - map_origin.x)/map_resolution)
-            grid_y = int((self.current_position.y - map_origin.y)/map_resolution)
-            grid_index = grid_y * map_width + grid_x
+        #publish la heatmap en tant qu'image
+        self.publish_radiation_heatmap()
+    
+    def update_local_radiation_map(self):
+        #centre la carte pr/r au robot...
+        center_x = self.map_size // 2
+        center_y = self.map_size // 2
 
-            if 0 <= grid_index < len(self.map.data):
-                self.map.header.stamp = self.get_clock().now().to_msg()
-                self.map.data = list(self.map.data) 
-                self.map.data[grid_index] = int(round(self.current_radiation * 100))
-                self.radiation_map_publisher.publish(self.map)
+        #converti la radiation en intensité (ex. sensor detcte 0.7 alors 70... car probleme quand pas un int donc *100 résout ca)
+        intensity = min(100, self.current_radiation * 100)
+
+        #self.get_logger().info(f"Max radiation in map before update: {np.max(self.radiation_map_local):.5f}")
+        
+        #ajouter radiation détecté à la position centrale
+        self.radiation_map_local[center_y, center_x] = intensity
+        
+        #self.get_logger().info(f"Max radiation in map after update: {np.max(self.radiation_map_local):.5f}")
+
+
+    def publish_radiation_heatmap(self):
+        heatmap = self.radiation_map_local.copy()
+
+        heatmap = np.clip(heatmap, 0, 100) #à retester mais cette ligne devrait ne plus etre pertinente
+        heatmap = (heatmap / 100) * 255 #transforme entre des valeur de 0 à 255 ârce que Opencv oblige ca
+        heatmap = heatmap.astype(np.uint8) #converti en entier car cest demander pour affichage d,image...
+
+        #apppliquer de la couleur pour plus de réaliste... un peu comme une heatmap 
+        heatmap_colored = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+        """ Légende des couleurs de  COLORMAP_JET:
+        Bleu = faible radiation
+        Vert = radiation moyenne
+        Rouge/jaune = forte radiation """
+
+        # Conversion en message ROS
+        heatmap_msg = self.bridge.cv2_to_imgmsg(heatmap_colored, encoding="bgr8")
+        heatmap_msg.header.stamp = self.get_clock().now().to_msg()
+        heatmap_msg.header.frame_id = "odom"  
+
+        self.heatmap_publisher.publish(heatmap_msg)
+        
 
 def main(args=None):
     rclpy.init(args=args)
--- src/rove_radiation/rove_radiation/radiation_publisher.py
+++ src/rove_radiation/rove_radiation/radiation_publisher.py
@@ -1,19 +1,76 @@
+import math
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
+from nav_msgs.msg import Odometry
+from visualization_msgs.msg import Marker
 
 class RadiationPublisher(Node):
     def __init__(self):
         super().__init__('radiation_publisher')
-        self.publisher = self.create_publisher(Float32, '/dose_rate', 10)
-        timer_period = 0.5
-        self.timer = self.create_timer(timer_period, self.publish_radiation_data)
+        self.source_x = 4.7071488
+        self.source_y = 0.0105071
+        self.source2_x = -6.288366
+        self.source2_y = -0.0121495
+
+        self.max_radiation = 0.87  #valeur maximum que la radiation peut avoir... donc cest la valeur de radiation quand on est pile poil sur le x et y de la radiation
+        self.decay_factor = 1.0   #Plus la valeur est garnde plus ca descend vite quand on seloinge
+
+        self.odom_subscription = self.create_subscription(Odometry, '/odometry/local', self.odom_callback, 10)
+        self.radiation_publisher = self.create_publisher(Float32, '/dose_rate', 10)
         #self.get_logger().info('Radiation publisher node has started.')
+
+        self.marker_publisher = self.create_publisher(Marker, "/radiation_sources", 10)
+        self.publish_radiation_markers()
     
+    def odom_callback(self, msg):
+        robot_x = msg.pose.pose.position.x
+        robot_y = msg.pose.pose.position.y
+
+        #Calcul pour calculer distance entre robot et source de radiation (distance euclidienne)
+        distance = math.sqrt((robot_x - self.source_x) ** 2 + (robot_y - self.source_y) ** 2)
+        distance2 = math.sqrt((robot_x - self.source2_x) ** 2 + (robot_y - self.source2_y) ** 2)
+
+        #formule de decroissance exponetielle selon distance
+        radiation_value = self.max_radiation * math.exp(-self.decay_factor * distance)
+        radiation_value2 = self.max_radiation * math.exp(-self.decay_factor * distance2)
+
+        # Publier la valeur de radiation
+        msg_radiation = Float32()
+        msg_radiation.data = radiation_value + radiation_value2
+        self.radiation_publisher.publish(msg_radiation)
+
+    def publish_radiation_markers(self):
+        #FONCTIONNE PAS""" JVOIS AUCUN MARKER DONC A REVOIR
+        #pour montrer les sources de radiations (slmt pour testage...a ctually toute cette classe cest slmt pour tester... en vrai elle sert a rien)
+        sources = [(4.7071488, 0.0105071), (-6.288366, -0.0121495)]  #coords des sources de raditioaons
+        
+        for i, (x, y) in enumerate(sources):
+            marker = Marker()
+            marker.header.frame_id = "map" #jsuis jamais sur si je dois mettre map ou odom à clarifier dans un futur proche
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.ns = "radiation_sources"
+            marker.id = i
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
+            marker.pose.position.x = x
+            marker.pose.position.y = y
+            marker.pose.position.z = 0.0  
+            marker.scale.x = 0.5
+            marker.scale.y = 0.5
+            marker.scale.z = 0.5
+            marker.color.a = 1.0 
+            marker.color.r = 1.0 
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+
+            self.marker_publisher.publish(marker)
+
     def publish_radiation_data(self):
-        msg = Float32()
-        msg.data = 0.35 # à remplacer par la vraie valeur
-        self.publisher.publish(msg)
+        pass
+        """ msg = Float32()
+        msg.data = 0.0 # à remplacer par la vraie valeur
+        self.publisher.publish(msg) """
         #self.get_logger().info(f'Publishing radiation data: {msg.data}')
     
 def main(args=None):
