55b6b23357d9025e5f010226bb198cb9a593aa13
patates-cipsi418 <denis.turk2001@outlook.com>
2025-03-24 17:05:36 -0400

    rename config for the two camera modes

--- src/rove_bringup/launch/camera.launch.py
+++ src/rove_bringup/launch/camera.launch.py
@@ -7,14 +7,37 @@ def generate_launch_description():
     dir = get_package_share_directory(package_name)
     # Get params files
     
-    insta360_x4 = Node(
-            package='usb_cam',
-            executable='usb_cam_node_exe',
-            name='usb_cam_node_exe',
-            output='screen',
-            parameters=[os.path.join(dir, 'config', 'insta360_x4.yaml')],
-        )
+    insta360_x4_wide = Node(
+        package='usb_cam',
+        executable='usb_cam_node_exe',
+        name='usb_cam_node_exe',
+        output='screen',
+        parameters=[os.path.join(dir, 'config', 'insta360_x4_wide.yaml')],
+        remappings = [
+            ('image_raw', f'insta360/image_raw'),
+            ('image_raw/compressed', f'insta360/image_compressed'),
+            ('image_raw/compressedDepth', f'insta360/compressedDepth'),
+            ('image_raw/theora', f'insta360/image_raw/theora'),
+            ('camera_info', f'insta360/camera_info'),
+        ]
+    )
+    
+    insta360_x4_split = Node(
+        package='usb_cam',
+        executable='usb_cam_node_exe',
+        name='usb_cam_node_exe',
+        output='screen',
+        parameters=[os.path.join(dir, 'config', 'insta360_x4_split.yaml')],
+        remappings = [
+            ('image_raw', f'insta360/image_raw'),
+            ('image_raw/compressed', f'insta360/image_compressed'),
+            ('image_raw/compressedDepth', f'insta360/compressedDepth'),
+            ('image_raw/theora', f'insta360/image_raw/theora'),
+            ('camera_info', f'insta360/camera_info'),
+        ]
+    )
 
     return LaunchDescription([
-        insta360_x4
+        insta360_x4_wide,
+        # insta360_x4_split
     ])
