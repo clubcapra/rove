8403522a5a7e50775f1768c09f5c0a08b1b495c9
patates-cipsi418 <denis.turk2001@outlook.com>
2025-05-22 21:39:06 -0400

    add back camera and rename package

--- src/rove_camera/launch/camera.launch.py
+++ src/rove_camera/launch/camera.launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 def generate_launch_description():
-    package_name = 'rove_insta_x4'
+    package_name = 'rove_camera'
     dir = get_package_share_directory(package_name)
     # Get params files
     
@@ -12,7 +12,7 @@ def generate_launch_description():
         executable='usb_cam_node_exe',
         name='usb_cam_node_exe',
         output='screen',
-        parameters=[os.path.join(dir, 'config', 'wide.yaml')],
+        parameters=[os.path.join(dir, 'config', 'x4_wide.yaml')],
         remappings = [
             ('image_raw', f'insta360/image_raw'),
             ('image_raw/compressed', f'insta360/image_compressed'),
@@ -22,18 +22,33 @@ def generate_launch_description():
         ]
     )
     
-    split = Node(
+    # split = Node(
+    #     package='usb_cam',
+    #     executable='usb_cam_node_exe',
+    #     name='usb_cam_node_exe',
+    #     output='screen',
+    #     parameters=[os.path.join(dir, 'config', 'x4_split.yaml')],
+    #     remappings = [
+    #         ('image_raw', f'insta360/image_raw'),
+    #         ('image_raw/compressed', f'insta360/image_compressed'),
+    #         ('image_raw/compressedDepth', f'insta360/compressedDepth'),
+    #         ('image_raw/theora', f'insta360/image_raw/theora'),
+    #         ('camera_info', f'insta360/camera_info'),
+    #     ]
+    # )
+    
+    back_camera = Node(
         package='usb_cam',
         executable='usb_cam_node_exe',
         name='usb_cam_node_exe',
         output='screen',
-        parameters=[os.path.join(dir, 'config', 'split.yaml')],
+        parameters=[os.path.join(dir, 'config', 'back_camera.yaml')],
         remappings = [
-            ('image_raw', f'insta360/image_raw'),
-            ('image_raw/compressed', f'insta360/image_compressed'),
-            ('image_raw/compressedDepth', f'insta360/compressedDepth'),
-            ('image_raw/theora', f'insta360/image_raw/theora'),
-            ('camera_info', f'insta360/camera_info'),
+            ('image_raw', f'back_camera/image_raw'),
+            ('image_raw/compressed', f'back_camera/image_compressed'),
+            ('image_raw/compressedDepth', f'back_camera/compressedDepth'),
+            ('image_raw/theora', f'back_camera/image_raw/theora'),
+            ('camera_info', f'back_camera/camera_info'),
         ]
     )
     
@@ -48,7 +63,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        wide,
-        #split,
-        image_splitter
+        # wide,
+        # image_splitter,
+        back_camera,
     ])
