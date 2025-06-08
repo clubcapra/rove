03d668499751c45f1a601703253c43cf076ecf31
nathan-gt <nathan.gt.protic@gmail.com>
2025-06-08 17:32:14 -0400

    Revert "Merge branch 'insta360_x4' into live"
    This reverts commit f9a93bf79524257e9d00627eee195e704f7b0d9d, reversing
    changes made to 6652aeb3a13d9ec5d9911ca3182db0e179a35409.

--- src/rove_bringup/launch/sim.launch.py
+++ src/rove_bringup/launch/sim.launch.py
@@ -3,110 +3,109 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
+from math import pi
 
 
 def generate_launch_description():
     # Get the launch directory
-    pkg_rove_bringup = get_package_share_directory("rove_bringup")
-    pkg_rove_description = get_package_share_directory("rove_description")
-    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
-    pkg_ovis = get_package_share_directory("ovis_bringup")
+    pkg_rove_bringup = get_package_share_directory('rove_bringup')
+    pkg_rove_description = get_package_share_directory('rove_description')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    # Get the URDF file
+    urdf_path = os.path.join(pkg_rove_description, 'urdf', 'rove.urdf.xacro')
+    robot_desc = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
 
     # Get simulation file
-    world_file_name = "worlds/base_world.world"
+    world_file_name = 'worlds/base_world.world'
     world = os.path.join(pkg_rove_description, world_file_name)
 
     # Setup to launch the simulator and Gazebo world
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
-        ),
-        launch_arguments={"gz_args": "-v 4 -r " + world}.items(),
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': "-v 4 -r " + world}.items(),
     )
 
-    walls_file_path = os.path.join(pkg_rove_description, "worlds", "walls.sdf")
+    walls_file_path = os.path.join(pkg_rove_description, 'worlds', 'walls.sdf')
     spawn_walls = Node(
-        package="ros_gz_sim",
-        executable="create",
-        arguments=[
-            "-file",
-            walls_file_path,
-            "-name",
-            "walls",
-            "-x",
-            "0",
-            "-y",
-            "0",
-            "-z",
-            "0",
-        ],
-        output="screen",
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-file', walls_file_path,
+                   '-name', 'walls',
+                   '-x', '0',
+                   '-y', '0',
+                   '-z', '0'],
+        output='screen',
     )
 
-    actor_file_path = os.path.join(pkg_rove_description, "worlds", "actor.sdf")
+    actor_file_path = os.path.join(pkg_rove_description, 'worlds', 'actor.sdf')
     spawn_actor = Node(
-        package="ros_gz_sim",
-        executable="create",
-        arguments=[
-            "-file",
-            actor_file_path,
-            "-name",
-            "actor",
-            "-x",
-            "0",
-            "-y",
-            "0",
-            "-z",
-            "0.06",
-        ],
-        output="screen",
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-file', actor_file_path,
+                   '-name', 'actor',
+                   '-topic', 'actor_pose',
+                   '-x', '0',
+                   '-y', '0',
+                   '-z', '0.1'],
+        output='screen',
     )
+
+    yaw = -pi / 2
 
     # Spawn robot
     spawn_rove = Node(
-        package="ros_gz_sim",
-        executable="create",
+        package='ros_gz_sim',
+        executable='create',
         arguments=[
-            "-name",
-            "rove",
-            "-topic",
-            "robot_description",
-            "-x",
-            "0",
-            "-y",
-            "0",
-            "-z",
-            "0.1",
+            '-name', 'rove',
+            '-topic', 'robot_description',
+            '-x', '0',
+            '-y', '0',
+            '-z', '0.1',
+            '-Y', str(yaw),
         ],
-        output="screen",
+        output='screen',
+    )
+
+    # Takes the description and joint angles as inputs and publishes
+    # the 3D poses of the robot links
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[
+            {'robot_description': robot_desc},
+            {"use_sim_time": True, }
+        ]
     )
 
     # fake human tracker
     human_tracker = Node(
-        package="rove_navigation",
-        executable="green_person_tracker",
-        name="green_person_tracker",
-        output="screen",
+            package='rove_navigation',
+            executable='green_person_tracker',
+            name='green_person_tracker',
+            output='screen',
     )
 
     # Bridge ROS topics and Gazebo messages for establishing communication
     bridge = Node(
-        package="ros_gz_bridge",
-        executable="parameter_bridge",
-        parameters=[
-            {
-                "config_file": os.path.join(
-                    pkg_rove_description, "config", "default_bridge.yaml"
-                ),
-                "qos_overrides./tf_static.publisher.durability": "transient_local",
-                "use_sim_time": True,
-            }
-        ],
-        output="screen",
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+            'config_file': os.path.join(pkg_rove_description, 'config',
+                                        'default_bridge.yaml'),
+            'qos_overrides./tf_static.publisher.durability': 'transient_local',
+            "use_sim_time": True,
+        }],
+        output='screen'
     )
 
     common = IncludeLaunchDescription(
@@ -118,29 +117,13 @@ def generate_launch_description():
         }.items(),
     )
 
-
-    ovis = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_ovis, "launch", "sim.launch.py"),
-        ),
-        launch_arguments={
-            "with_rove": "true",
-            "with_joy": "false",
-            "ovis_base_origin": "0.22 0 0.34 0 0 3.14",
-        }.items(),
-    )
-
-
-
-    return LaunchDescription(
-        [
+    return LaunchDescription([
             gz_sim,
             bridge,
+            robot_state_publisher,
             spawn_walls,
             spawn_actor,
             spawn_rove,
             common,
-            # human_tracker,
-            ovis,
-        ]
-    )
+            human_tracker,
+            ])
--- src/rove_navigation/setup.py
+++ src/rove_navigation/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
-import os
 from glob import glob
+import os
 
 package_name = 'rove_navigation'
 
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
         ('share/' + package_name + '/config', glob('config/*')),
     ],
     install_requires=['setuptools'],
@@ -22,6 +22,7 @@ setup(
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
+    package_dir={'': 'src'},
     entry_points={
         'console_scripts': [
             'navigate_to_person = rove_navigation.navigate_to_person:main',
@@ -32,8 +33,6 @@ setup(
             'exploration = rove_navigation.exploration:main',
             'lost_connection = rove_navigation.lost_connection:main',
             'mule_behavior = rove_navigation.mule_behavior:main',
-            'ovis_pull = rove_navigation.ovis_pull:main',
-            'model_attacher = rove_navigation.model_attacher:main',
         ],
     },
 )
