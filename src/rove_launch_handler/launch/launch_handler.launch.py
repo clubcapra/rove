2e0a7907410b743c8ff00644d74e347e2ef2bd64
nathan-gt <nathan.gt.protic@gmail.com>
2025-06-12 15:56:03 -0400

    Fixed passing roboguard topics to foxglove

--- src/rove_launch_handler/launch/launch_handler.launch.py
+++ src/rove_launch_handler/launch/launch_handler.launch.py
@@ -14,12 +14,15 @@ def generate_launch_description():
 
     return LaunchDescription([
         IncludeLaunchDescription(
-            XMLLaunchDescriptionSource(rosbridge_launch_file)
+            XMLLaunchDescriptionSource(rosbridge_launch_file),
+            launch_arguments=[
+                ("include_hidden", "true"),
+            ],
         ),
         Node(
             package='rove_launch_handler',
             executable='launch_handler.py',
             name='launch_handler',
-            output='screen'
+            output='screen',
         )
     ])
