07fc58e0a7d222069bef93d9c9abb1edde6f76a8
nathan-gt <nathan.gt.protic@gmail.com>
2025-06-13 15:28:30 -0400

    Added a monitoring script for roboguard to ensure foxglove detects its topics. Updated the foxglove ui

--- src/rove_launch_handler/scripts/update_service.sh
+++ src/rove_launch_handler/scripts/update_service.sh
@@ -2,11 +2,11 @@
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-systemctl disable --now --user launch_script.service
-# sudo cp $script_dir/launch_script.service /etc/systemd/user/launch_script.service
-# systemctl start --user launch_script.service
 
 sudo systemctl disable --now launch_script.service
+sudo systemctl disable --now launch_script_monitor.service
 sudo cp $script_dir/launch_script.service /etc/systemd/system/launch_script.service
+sudo cp $script_dir/launch_script_monitor.service /etc/systemd/system/launch_script_monitor.service
 sudo systemctl daemon-reload
 sudo systemctl enable --now launch_script.service
+sudo systemctl enable --now launch_script_monitor.service
