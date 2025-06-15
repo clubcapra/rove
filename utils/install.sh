6798f41e7d351606284abe34e62f8c180e033df5
nathan-gt <nathan.gt.protic@gmail.com>
2025-06-14 21:04:33 -0400

    adding rule

--- utils/install.sh
+++ utils/install.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+
 sudo mkdir -p /media/SSD/stable/log/ros2
 sudo chmod 775 /media/SSD/stable/log/ros2
 sudo chown rove:rove /media/SSD/stable/log/ros2
-sudo chmod +x ./src/rove_launch_handler/scripts/update_service.sh
-./src/rove_launch_handler/scripts/update_service.sh
+sudo chmod +x $script_dir/../src/rove_launch_handler/scripts/update_service.sh
+sudo cp $script_dir/99-vn300.rules /etc/udev/rules.d/99-vn300.rules
+$script_dir/../src/rove_launch_handler/scripts/update_service.sh
